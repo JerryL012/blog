c1e0a924a4007e8619e7217c986a91c6b23597b1
Ray <873762491@qq.com>
2020-06-27 16:38:15 +0100

    basic blog page

--- blog/settings.py
+++ blog/settings.py
@@ -106,3 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'src/static')]
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
