9a3a4b93418dd05251b7bc4898f5d8fa2c2c24cd
Ray <873762491@qq.com>
2020-06-29 12:41:31 +0100

    done post page, latest posts on the right, category count

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from follow.models import Follow
-from django.contrib.auth.forms import AuthenticationForm
+# all we need to paginate query sets
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 
+def get_category_count():
+    # all the categories of posts. annotate returns a dict where each key is each category
+    #Count() counts the num of posts whose category is category
+    queryset = Post.objects.values('categories__title').annotate(Count('categories__title'))
+    return queryset
 
 def index(request):
     # grab the post with true featured
@@ -22,7 +29,31 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
-    return render(request, 'blog.html', {})
+    category_count = get_category_count()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    post_list = Post.objects.all()
+    # how many it should hold in one page
+    paginator = Paginator(post_list, 4)
+    # page passed in the url, like ?page==1. page is the page number, 1234...
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    # check whether the page is empty or page parameter is wrong
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # go to the page 1
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        # return the num of pages. e.g 100 posts, per 4, so it should be 25 pages, the last page
+        paginated_queryset = paginator.page(paginator.num_pages)
 
-def post(request):
+    context = {
+        'queryset': paginated_queryset,
+        'page_request_var': page_request_var,
+        'most_recent': most_recent,
+        'category_count': category_count
+    }
+    return render(request, 'blog.html', context)
+
+def post(request, id):
     return render(request, 'post.html', {})
