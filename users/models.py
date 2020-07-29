5d1ba8428f0af569fd800b99401ed0becd173957
Ray <873762491@qq.com>
2020-07-29 21:53:57 +0100

    user update profile and save it to database

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+# Pillow package
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +10,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}'
+    # overwrite func to resize image
+    def save(self):
+        # save the large image
+        super().save()
+        # open the image of the current instance
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
